f4969a7ab447bc5270339695e0a9388f2e085763
michbur <michalburdukiewicz@gmail.com>
2015-08-20 16:48:52 +0200

    added benchmark for plasmodium

---
+++ plasmodium_benchmark/read_signalHsmm3.R
@@ -0,0 +1,5 @@
+dat <- do.call(rbind, strsplit(readLines("signalP30.txt")[-1], " +"))[, c(3, 13, 17, 19)]
+res <- t(apply(dat, 1, as.numeric))
+#predictions for signalP 3.0
+preds <- round(res[, c(2, 4)], 2) > 0.5
+colnames(preds) <- c("signalP3nn", "signalP3hmm")

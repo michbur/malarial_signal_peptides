ef4d3f02e990e0eb00adc5d3c1c9680404b80dc2
michbur <michalburdukiewicz@gmail.com>
2016-01-21 14:10:49 +0100

    need to update nhc2 model

--- find_nhc2.R
+++ find_nhc2.R
@@ -2,7 +2,7 @@ seq50_10 <- read_uniprot("./training_data/sp1950_2010.txt", ft_names = "signal")
 
 t(sapply(seq50_10, find_nhc))
 
-find_nhc2 <- function(protein, signal = NULL) {
+find_nhc4 <- function(protein, signal = NULL) {
     protein <- toupper(protein)
     if (is.null(signal)) 
       signal <- attr(protein, "signal")
@@ -11,31 +11,40 @@ find_nhc2 <- function(protein, signal = NULL) {
     
     start_c <- signal[2] - 4
     
-    start_h <- start_c - 6
-    #if statement to prevent negative values
-    if (start_h > 1) {
-      #nonh number of nonhydrophobic residues
-      nonh <- 0
-      #noc number of charged
-      noc <- 0
-      while(nonh < 3 && noc == 0 && start_h > 1) {
-        start_h <- start_h - 1
-        nonh <- nonh + ifelse(sig[start_h] %in% c("A", "I", "L", "M", "F", "W", "V"), -1, 1)
-        nonh <- ifelse(nonh < 0, 0, nonh)
-        noc <- ifelse(sig[start_h] %in% c("R", "H", "K", "D", "E"), 1, 0)
-      }
-    } else {
-      start_h <- 1
+    start_n <- 1
+    start_h <- 2
+    noh <- 0
+    
+    while(noh < 3 && start_h < start_c) {
+      start_h <- start_h + 1
+      noh <- noh + ifelse(sig[start_h] %in% c("F", "I", "L", "V", "W"), 1, 0)
+      noh <- ifelse(noh < 1, 0, noh)
     }
     
-    prestart_c <- start_c - 1
-    noh <- 0
-    while(noh == 0 && start_h < prestart_c) {
-      start_h <- start_h + 1
-      noh <- noh + ifelse(sig[start_h] %in% c("A", "I", "L", "M", "F", "W", "V"), 1, 0)
-    }
-    #c(start_n = signal[1], start_h = start_h, start_c = start_c, cs = signal[2])
+    start_h <- start_h - 3
+    
     c(start_n = 1, start_h = start_h, start_c = start_c, cs = signal[2])
 }
 
-t(sapply(seq50_10, find_nhc2))
+
+
+
+find_nhc3 <- function(protein, signal = NULL) {
+  protein <- toupper(protein)
+  if (is.null(signal)) 
+    signal <- attr(protein, "signal")
+  
+  protein[(signal[1] + 1):(signal[1] + 9)]
+}
+
+sigs <- sapply(seq50_10, function(protein)
+  attr(protein, "signal")[2])
+
+sapply(seq50_10[sigs == 19], find_nhc3) %>% t %>% 
+  apply(2, function(i) table(factor(i, levels = a()[-1]))) %>%
+  melt %>%
+  ggplot(aes(x = Var1, y = value, fill = Var1)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ Var2)
+
+t(sapply(seq50_10, find_nhc4))

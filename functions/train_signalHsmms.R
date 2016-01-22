c94808da6f2e6276afe94c5f5c667b437ce2fcdd
michbur <michalburdukiewicz@gmail.com>
2016-01-22 11:43:46 +0100

    trying to valid different nhc models

--- functions/train_signalHsmms.R
+++ functions/train_signalHsmms.R
@@ -14,21 +14,17 @@ train_signalHsmms <- function(seqs) {
   
   # no degeneration
   signalHsmmNODEG <- train_hsmm(seqs, aas)
-  signalHsmmNOCNODEG <- train_hsmm(seqs, aas, region_fun = find_nhc4)
-  
+
   # degeneration
   signalHsmm <- train_hsmm(seqs, aaaggregation)
-  #signalHsmmNOC <- train_hsmm(seqs, aaaggregation, region_fun = find_nhc4)
-  
+
   #homology 50
   seq50f <- cdhit(seqs, thresh = 0.5, word_length = 2, only_signal = TRUE)
   signalHsmmNOHOM50 <- train_hsmm(seqs[seq50f], aaaggregation)
-  #signalHsmmNOCNOHOM50 <- train_hsmm(seqs, aaaggregation, region_fun = find_nhc4)
   
   #homology 90
   seq90f <- cdhit(seqs, thresh = 0.9, word_length = 5, only_signal = TRUE)
   signalHsmmNOHOM90 <- train_hsmm(seqs[seq90f], aaaggregation)
-  #signalHsmmNOCNOHOM90 <- train_hsmm(seqs[seq90f], aaaggregation, region_fun = find_nhc4)
   
   list(signalHsmm = signalHsmm,
        signalHsmmNODEG = signalHsmmNODEG,
---
+++ test_nhc.R
@@ -0,0 +1,87 @@
+require(XML)
+require(seqinr)
+require(fitdistrplus)
+require(dplyr)
+require(signalHsmm)
+require(hmeasure)
+require(pbapply)
+require(reshape2)
+require(hmeasure)
+require(xtable)
+require(biogram)
+require(ggplot2)
+require(grid)
+require(gridExtra)
+
+if(Sys.info()["nodename"] == "phobos" )
+  pathway <- "/home/michal/Dropbox/signal-peptide2_data/"
+
+if(Sys.info()["nodename"] == "MICHALKOMP" )
+  pathway <- "C:/Users/Michal/Dropbox/signal-peptide2_data/"
+
+source("./functions/plot_tools.R")
+source("./functions/reglen_plot.R")
+source("./functions/cv_analysis.R")
+source("./functions/enc_region.R")
+source("./functions/benchmark_functions.R")
+source("./functions/cdhit.R")
+source("./functions/train_signalHsmms.R")
+source("./functions/signalHsmm_kmer.R")
+
+find_nhc2 <- function(protein, signal = NULL) {
+  protein <- toupper(protein)
+  if (is.null(signal)) 
+    signal <- attr(protein, "signal")
+  
+  sig <- protein[signal[1]:signal[2]]
+  
+  start_c <- signal[2] - 4
+  
+  start_n <- 1
+  start_h <- 2
+  noh <- 0
+  
+  while(noh < 4 && start_h < start_c) {
+    start_h <- start_h + 1
+    noh <- noh + ifelse(sig[start_h] %in% c("F", "I", "L", "V", "W"), 1, 0)
+    noh <- ifelse(noh < 1, 0, noh)
+  }
+  
+  start_h <- start_h - 4
+  
+  c(start_n = 1, start_h = start_h, start_c = start_c, cs = signal[2])
+}
+
+sapply(seq50_10, find_nhc2)
+find_nhc2(seq50_10[[2031]])
+
+train_signalHsmms_nhc <- function(seqs) {
+  #iterations without degeneration
+  #aas <- tolower(a()[-1])
+  aas <- a()[-1]
+  names(aas) <- 1L:20
+  
+  # no degeneration
+  #signalHsmmNODEG <- train_hsmm(seqs, aas)
+  signalHsmmNOCNODEG <- train_hsmm(seqs, aas, region_fun = find_nhc2)
+
+  list(signalHsmmNODEG = signalHsmmNODEG,
+       signalHsmmNOCNODEG = signalHsmmNOCNODEG)
+}
+
+
+
+signalHsmms10 <- train_signalHsmms_nhc(seq50_10[-2031])
+names(signalHsmms10) <- paste0(names(signalHsmms10), "_10")
+
+signalHsmms87 <- train_signalHsmms_nhc(seq50_87)
+names(signalHsmms87) <- paste0(names(signalHsmms87), "_87")
+
+other_pred_plas <- read_other_software("./plasmodium_benchmark_results_NOHOM")
+signalHsmm_pred_plas <- get_signalHsmm_preds(c(signalHsmms10, signalHsmms87),
+                                             "./plasmodium_benchmark_data/benchmark_plas_data_NOHOM.fasta")
+
+metrics_plas_NOHOM <- calc_metrics(c(rep(1, 51), rep(0, 211)), 
+                                   data.frame(other_pred_plas[["prob"]], signalHsmm_pred_plas[["prob"]]), 
+                                   0.5)
+metrics_plas_NOHOM[c("AUC", "MCC")]

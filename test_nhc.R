d1c47c813de84e6fe82624b6e3fcf2567d240057
michbur <michalburdukiewicz@gmail.com>
2016-01-22 12:49:03 +0100

    still having issues while testing different nhcs

--- test_nhc.R
+++ test_nhc.R
@@ -28,6 +28,7 @@ source("./functions/cdhit.R")
 source("./functions/train_signalHsmms.R")
 source("./functions/signalHsmm_kmer.R")
 
+# absurdly simple
 find_nhc2 <- function(protein, signal = NULL) {
   protein <- toupper(protein)
   if (is.null(signal)) 
@@ -52,8 +53,44 @@ find_nhc2 <- function(protein, signal = NULL) {
   c(start_n = 1, start_h = start_h, start_c = start_c, cs = signal[2])
 }
 
-sapply(seq50_10, find_nhc2)
-find_nhc2(seq50_10[[2031]])
+find_nhc3 <- function(protein, signal = NULL) {
+  protein <- toupper(protein)
+  if (is.null(signal)) 
+    signal <- attr(protein, "signal")
+  
+  sig <- protein[signal[1]:signal[2]]
+  
+  start_c <- signal[2] - 4
+  
+  start_h <- start_c - 6
+  #if statement to prevent negative values
+  if (start_h > 1) {
+    #nonh number of nonhydrophobic residues
+    nonh <- 0
+    #noc number of charged
+    noc <- 0
+    while(nonh < 3 && noc == 0 && start_h > 1) {
+      start_h <- start_h - 1
+      nonh <- nonh + ifelse(sig[start_h] %in% c("A", "I", "L", "M", "F", "W", "V"), -1, 1)
+      nonh <- ifelse(nonh < 0, 0, nonh)
+      noc <- ifelse(sig[start_h] %in% c("R", "H", "K", "D", "E"), 1, 0)
+    }
+  } else {
+    start_h <- 1
+  }
+  
+  prestart_c <- start_c - 1
+  noh <- 0
+  while(noh == 0 && start_h < prestart_c) {
+    start_h <- start_h + 1
+    noh <- noh + ifelse(sig[start_h] %in% c("A", "I", "L", "M", "F", "W", "V"), 1, 0)
+  }
+  #c(start_n = signal[1], start_h = start_h, start_c = start_c, cs = signal[2])
+  c(start_n = 1, start_h = start_h, start_c = start_c, cs = signal[2])
+}
+
+# sapply(seq50_10, find_nhc2)
+# find_nhc2(seq50_10[[2031]])
 
 train_signalHsmms_nhc <- function(seqs) {
   #iterations without degeneration
@@ -62,15 +99,17 @@ train_signalHsmms_nhc <- function(seqs) {
   names(aas) <- 1L:20
   
   # no degeneration
-  #signalHsmmNODEG <- train_hsmm(seqs, aas)
-  signalHsmmNOCNODEG <- train_hsmm(seqs, aas, region_fun = find_nhc2)
-
+  signalHsmmNODEG <- train_hsmm(seqs, aas)
+  signalHsmmNODEG2 <- train_hsmm(seqs, aas, region_fun = find_nhc2)
+  signalHsmmNODEG3 <- train_hsmm(seqs, aas, region_fun = find_nhc3)
+  
   list(signalHsmmNODEG = signalHsmmNODEG,
-       signalHsmmNOCNODEG = signalHsmmNOCNODEG)
+       signalHsmmNODEG2 = signalHsmmNODEG2,
+       signalHsmmNODEG3 = signalHsmmNODEG3)
 }
 
 
-
+# removed single 7 aa long presequence
 signalHsmms10 <- train_signalHsmms_nhc(seq50_10[-2031])
 names(signalHsmms10) <- paste0(names(signalHsmms10), "_10")
 
